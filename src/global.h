d39adbd5a150f689b505245211db8ef6dd1dfc08
ambiennt <63216972+ambiennt@users.noreply.github.com>
2021-06-03 22:18:24 -0700

    update header

--- src/global.h
+++ src/global.h
@@ -16,15 +16,23 @@
 struct Settings {
 	float ProjectileYOffset = 0.0;
 	float ProjectileZOffset = 0.0;
+	bool FishingRodOffsetEnabled = false;
 	float FishingRodYOffset = 0.0;
 	float FishingRodZOffset = 0.0;
+	bool SplashPotionOffsetEnabled = false;
+	float SplashPotionYOffset = 0.0;
+	float SplashPotionZOffset = 0.0;
 
 	template <typename IO> static inline bool io(IO f, Settings &settings, YAML::Node &node) { 
 		
 		return f(settings.ProjectileYOffset, node["ProjectileYOffset"]) &&
 			f(settings.ProjectileZOffset, node["ProjectileZOffset"]) &&
+			f(settings.FishingRodOffsetEnabled, node["FishingRodOffsetEnabled"]) &&
 			f(settings.FishingRodYOffset, node["FishingRodYOffset"]) &&
-			f(settings.FishingRodZOffset, node["FishingRodZOffset"]);
+			f(settings.FishingRodZOffset, node["FishingRodZOffset"]) &&
+			f(settings.SplashPotionOffsetEnabled, node["SplashPotionOffsetEnabled"]) &&
+			f(settings.SplashPotionYOffset, node["SplashPotionYOffset"]) &&
+			f(settings.SplashPotionZOffset, node["SplashPotionZOffset"]);
 	}
 };
 
